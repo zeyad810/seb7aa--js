dd2b923046bfc4af20e5d06cbe718c7fee865d98
زياد اشرف محمد ابراهيم شرف الدين <115799167+zeyad810@users.noreply.github.com>
2024-01-14 16:29:15 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+let count1 = 0;
+let count_1 =document.getElementById("count-1");
+function increment1() {
+    count1 = count1 + 1;
+    count_1.innerText=count1;
+}
+let count2 =0;
+let count_2 =document.getElementById("count-2");
+function increment2() {
+    count2 = count2 + 1;
+    count_2.innerText=count2;
+}
+let count3 =0;
+let count_3 =document.getElementById("count-3");
+function increment3() {
+    count3 = count3 + 1;
+    count_3.innerText=count3;
+}
+let count4 =0;
+let count_4 =document.getElementById("count-4");
+function increment4() {
+    count4 = count4 + 1;
+    count_4.innerText=count4;
+}
